805b8386f517fd4ef7ff7ce4b98aa68fb403fb9b
paryyongwoo <apfhd5620@gmail.com>
2017-10-05 01:15:49 +0900

    10-05 PYW 팔레트 다이얼로그 만드는중

--- app/src/main/java/com/example/tnrkd/colormakey/PaletteActivity.java
+++ app/src/main/java/com/example/tnrkd/colormakey/PaletteActivity.java
@@ -2,9 +2,11 @@ package com.example.tnrkd.colormakey;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
 
 import com.example.tnrkd.colormakey.adapter.ColorGridPaletteAdapter;
+import com.example.tnrkd.colormakey.dialog.GalleryCameraDialog;
 import com.google.firebase.database.DatabaseReference;
 
 /**
@@ -16,6 +18,8 @@ public class PaletteActivity extends Activity {
     private final String TAG = "PaletteActivity";
     private GridView gridView;
 
+    private GalleryCameraDialog galleryCameraDialog;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,4 +30,29 @@ public class PaletteActivity extends Activity {
         gridView.setAdapter(adapter);
 
     }
+
+    public void onClickView(View v) {
+        switch (v.getId()) {
+            case R.id.color_add_button:
+                galleryCameraDialog = new GalleryCameraDialog(this,
+                        "[다이얼로그 제목]", // 제목
+                        "다이얼로그 내용 표시하기", // 내용
+                        leftListener, // 왼쪽 버튼 이벤트
+                        rightListener); // 오른쪽 버튼 이벤트
+                galleryCameraDialog.show();
+                break;
+        }
+    }
+
+    private View.OnClickListener leftListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            galleryCameraDialog.dismiss();
+        }
+    };
+
+    private View.OnClickListener rightListener = new View.OnClickListener() {
+        public void onClick(View v) {
+        }
+    };
+
 }
---
+++ app/src/main/java/com/example/tnrkd/colormakey/dialog/GalleryCameraDialog.java
@@ -0,0 +1,69 @@
+package com.example.tnrkd.colormakey.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.tnrkd.colormakey.R;
+
+/**
+ * Created by XNOTE on 2017-10-05.
+ */
+
+public class GalleryCameraDialog extends Dialog {
+
+    private TextView titleTextView;
+    private TextView contentTextView;
+    private ImageButton galleryImageButton;
+    private ImageButton cameraImageButton;
+
+    private String title;
+    private String content;
+
+    private View.OnClickListener mLeftClickListener;
+    private View.OnClickListener mRightClickListener;
+
+    // 클릭버튼이 확인과 취소 두개일때 생성자 함수로 이벤트를 받는다
+    public GalleryCameraDialog(Context context, String title, String content, View.OnClickListener leftListener, View.OnClickListener rightListener) {
+        super(context, android.R.style.Theme_Translucent_NoTitleBar);
+        this.title = title;
+        this.content = content;
+        this.mLeftClickListener = leftListener;
+        this.mRightClickListener = rightListener;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 다이얼로그 외부 화면 흐리게 표현
+        WindowManager.LayoutParams lpWindow = new WindowManager.LayoutParams();
+        lpWindow.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        lpWindow.dimAmount = 0.8f;
+        getWindow().setAttributes(lpWindow);
+
+        setContentView(R.layout.dialog_gallery_camera);
+
+        titleTextView = (TextView) findViewById(R.id.txt_title);
+        contentTextView = (TextView) findViewById(R.id.txt_content);
+        galleryImageButton = findViewById(R.id.gallery_button);
+        cameraImageButton = findViewById(R.id.camera_button);
+
+        // 제목과 내용을 생성자에서 셋팅한다.
+        titleTextView.setText(title);
+        contentTextView.setText(content);
+
+        // 클릭 이벤트 셋팅
+        if (mLeftClickListener != null && mRightClickListener != null) {
+            galleryImageButton.setOnClickListener(mLeftClickListener);
+            cameraImageButton.setOnClickListener(mRightClickListener);
+        }else {
+
+        }
+    }
+
+}
